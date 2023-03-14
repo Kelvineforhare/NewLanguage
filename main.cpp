7218a7f49768827facba5091d39cf86d17c2bbd0
Your Name <you@example.com>
2023-03-14 12:25:13 +0000

    Start to make file as input

--- main.cpp
+++ main.cpp
@@ -3,20 +3,33 @@
 #include "Include/token.hpp"
 #include "Include/parser.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-    //if and else required
-    //semi colons after if else stantments and while statements
-    //semi colons after all stantment
-    //only int is the type
+    if (argc > 1)
+    {
+
+        string t1;
+
+        string t2(argv[1]);
+
+        cout << "t1 :\n"
+             << t1 << std::endl;
+
+        cout << "t2 :\n"
+             << t2 << std::endl;
+    }
+    // if and else required
+    // semi colons after if else stantments and while statements
+    // semi colons after all stantment
+    // only int is the type
     string fib = "n = 1;"
                  "minus1 = 1;"
                  "minus2 = 0;"
                  "while (n > 0) {"
-                    "temp = minus2;"
-                    "minus2 = minus1 + minus2;"
-                    "minus1 = temp;"
-                    "n = n - 1;"
+                 "temp = minus2;"
+                 "minus2 = minus1 + minus2;"
+                 "minus1 = temp;"
+                 "n = n - 1;"
                  "};"
                  "print(minus2);";
     auto input = getTokensFromLang(fib);
